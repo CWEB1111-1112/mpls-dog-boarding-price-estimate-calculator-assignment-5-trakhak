33f485d17363d795383b5f68562280f6476cfa75
Tran <trakhak@dunwoody.edu>
2019-12-09 13:41:55 -0600

    estimate

--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
-
+//Khali Tran Assignment 5
 namespace assignment_five
 {
     class Program
     {
         static void Main(string[] args)
         {
-          //Create a three object instances of Estimate and output data using toString Method from Estimate class  
+          //Create a three object instances of Estimate and output data using toString Method from Estimate class 
+          //initializing variables
+          string dogOwnerName, dogName;
+          int dogWeight,numOfDays,SERVICECODE = "123", estimate;
+          //w
+          //w
         }
 
         //Create a class called Estimate
+    }
+
+    class Estimate
+    {
+
     }
 }
